768ae9ee1bf2f88cb881b60cfcb349b107ce37be
xthet <xapski2671@gmail.com>
2024-09-03 19:39:26 +0100

    ras

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import Layout from "@/components/layout"
 import "@/styles/globals.sass"
 import type { Metadata } from "next"
-import { Outfit } from "next/font/google"
+import { Outfit, Poppins } from "next/font/google"
 
 export const metadata: Metadata = {
 	title: "Ecopulse Technologies",
@@ -16,13 +16,20 @@ const outfit = Outfit({
 	weight: ["300", "400", "500", "600", "700", "800", "900"],
 })
 
+const poppins = Poppins({
+	display: "swap",
+	subsets: ["latin"],
+	variable: "--font-def",
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+})
+
 export default function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode
 }>) {
 	return (
-		<html lang="en" className={`${outfit.variable}`}>
+		<html lang="en" className={`${outfit.variable} ${poppins.variable}`}>
 			<body>
 				<Layout>{children}</Layout>
 			</body>
